12e56422af006bdfe96a687207f4cfb94a997828
Abhishek-aivonic <vuppala.saiabhishek@aivonic.com>
2025-12-31 16:57:29 +0530

    Modification in URL

--- client/src/services/chatService.ts
+++ client/src/services/chatService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_BASE_URL = process.env.REACT_APP_API_URL;
+const API_BASE_URL = "https://spur-ai-bot.onrender.com";
 
 export const sendMessage = async(userText:string,sid:string | null)=>{
     console.log(userText,sid);
@@ -12,6 +12,7 @@ export const sendMessage = async(userText:string,sid:string | null)=>{
         },{
             headers: { 'Content-Type': 'application/json' }
         });
+        console.log(response);
         return response; 
     } 
     catch (error) {
